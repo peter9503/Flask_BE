4c847f10cd39412fda726ab5cdbe141204167a0c
peter9503 <peter9503@gmail.com>
2021-09-29 12:04:24 +0800

    update model with uuid

--- model/account.py
+++ model/account.py
@@ -1,26 +1,27 @@
 from main import db
+from sqlalchemy.dialects.postgresql import UUID
 import hashlib
 import os
+import uuid
 
-nonce = os.getenv('NONCE')
+nonce = os.getenv('HASH_NONCE')
 
 # db Model create
 class account(db.Model):
     __tablename__ = 'account'
-    uid      = db.Column(db.Integer         , unique   = True, primary_key = True   )
-    username = db.Column(db.String(100)     , unique   = True, nullable    = False  )
-    password_hash = db.Column(db.String(256), nullable = False                     )
+    uid      = db.Column(UUID(as_uuid=True) , unique   = True, primary_key = True   , default=uuid.uuid4)
+    account  = db.Column(db.String(100)     , unique   = True, nullable    = False  )
+    password_hash = db.Column(db.String(256), nullable = False                      )
     email    = db.Column(db.String(100)     , unique   = True, nullable    = False  )
     isDelete = db.Column(db.Boolean         , nullable = False                      )
 
-    def __init__(self,uid,username,password, email, gender) -> None:
-        self.uid      = int(uid)
-        self.username = str(username)
+    def __init__(self,account,password, email) -> None:
+        self.account  = str(account)
         self.email    = str(email)
         self.isDelete = False
-        self.password_hash = hashlib.sha224(password + nonce).hexdigest()
+        self.password_hash = hashlib.sha224((password + nonce).encode('utf-8')).hexdigest()
 
     def valid_password(self, password) -> bool:
-        return hashlib.sha224(password + nonce).hexdigest() == self.password_hash
+        return hashlib.sha224((password + nonce).encode('utf-8')).hexdigest() == self.password_hash
 
     
